62046eb05d7adfcf5d819cb12f41aca94d130a1f
wanghao <whateverhow@gmail.com>
2023-03-26 19:54:37 -0500

    [master]   (use "git restore --staged <file>..." to unstage)

--- ops/updateProd.sh
+++ ops/updateProd.sh
@@ -1,11 +1,11 @@
 VERSION=$(git rev-parse HEAD)
 echo $VERSION
 
-gsed -i "s/bitmex-ws-orderbook\.git#.*\"/bitmex-ws-orderbook\.git#${VERSION}\"/g" ../bitmex_hf/package.json
-gsed -i "s/bitmex-ws-orderbook\.git#.*\"/bitmex-ws-orderbook\.git#${VERSION}\"/g" ../bitmex_hf/yarn.lock
+gsed -i "s/qs-ws-orderbook\.git#.*\"/qs-ws-orderbook\.git#${VERSION}\"/g" ../bitmex_hf/package.json
+gsed -i "s/qs-ws-orderbook\.git#.*\"/qs-ws-orderbook\.git#${VERSION}\"/g" ../bitmex_hf/yarn.lock
 
-gsed -i "s/bitmex-ws-orderbook\.git#.*\"/bitmex-ws-orderbook\.git#${VERSION}\"/g" ../ex-core/package.json
-gsed -i "s/bitmex-ws-orderbook\.git#.*\"/bitmex-ws-orderbook\.git#${VERSION}\"/g" ../ex-core/yarn.lock
+gsed -i "s/qs-ws-orderbook\.git#.*\"/qs-ws-orderbook\.git#${VERSION}\"/g" ../ex-core/package.json
+gsed -i "s/qs-ws-orderbook\.git#.*\"/qs-ws-orderbook\.git#${VERSION}\"/g" ../ex-core/yarn.lock
 
-gsed -i "s/bitmex-ws-orderbook\.git#.*\"/bitmex-ws-orderbook\.git#${VERSION}\"/g" ../ammarb/package.json
-gsed -i "s/bitmex-ws-orderbook\.git#.*\"/bitmex-ws-orderbook\.git#${VERSION}\"/g" ../ammarb/yarn.lock
+gsed -i "s/qs-ws-orderbook\.git#.*\"/qs-ws-orderbook\.git#${VERSION}\"/g" ../ammarb/package.json
+gsed -i "s/qs-ws-orderbook\.git#.*\"/qs-ws-orderbook\.git#${VERSION}\"/g" ../ammarb/yarn.lock
